d18be11a674701393054ae9f49a025e4fc318b3f
Kodiak Firesmith <firesmith@protonmail.com>
2021-04-18 15:50:11 -0400

    Initial debian variant support is working

--- generate_patch_facts.sh
+++ generate_patch_facts.sh
@@ -31,47 +31,72 @@ function discern_debvers() {
   repostring="$(egrep "^deb " /etc/apt/sources.list | grep "updates main")"
 
 
-  errata_support=True
+  errata_support=true
 
   case "$repostring" in
     *squeeze*)
       distro=debian
       distrovers=6
-      EOL=True
+      EOL=true
       ;;
     *wheezy*)
       distro=debian
       distrovers=7
-      EOL=True
+      EOL=true
       ;;
     *jessie*)
       distro=debian
       distrovers=8
-      EOL=True
+      EOL=true
       ;;
     *stretch*)
       distro=debian
       distrovers=9
-      EOL=False
+      EOL=false
       ;;
     *buster*)
       distro=debian
       distrovers=10
-      EOL=False
+      EOL=false
       ;;
     *lucid*)
       distro=ubuntu
       distrovers=10
-      EOL=True
+      EOL=true
+      ;;
+    *precise*)
+      distro=ubuntu
+      distrovers=12
+      EOL=true
+      ;;
+    *trusty*)
+      distro=ubuntu
+      distrovers=14
+      EOL=true
+      ;;
+    *xenial*)
+      distro=ubuntu
+      distrovers=16
+      EOL=true
+      ;;
+    *bionic*)
+      distro=ubuntu
+      distrovers=18
+      EOL=false
       ;;
     *focal*)
       distro=ubuntu
       distrovers=20
-      EOL=False
+      EOL=false
       ;;
     *groovy*)
       distro=ubuntu
       distrovers=20.10
+      EOL=false
+      ;;
+    *hirsute*)
+      distro=ubuntu
+      distrovers=21.04
       EOL=false
       ;;
     *)
@@ -79,10 +104,27 @@ function discern_debvers() {
   esac
 }
 
+function debian_check_updates() {
+  apt-get clean
+  if [ -f "/usr/lib/update-notifier/apt-check" ]; then
+    updatedata="$(/usr/lib/update-notifier/apt-check 2>&1)"
+    security_updates="$(echo $updatedata | cut -d";" -f2)"
+    all_updates="$(echo $updatedata | cut -d";" -f1)"
+  else
+    security_updates="$(apt-get upgrade -s | egrep '^Inst ' | grep -i security | wc -l)"
+    all_updates="$(apt-get upgrade -s | egrep '^Inst ' | wc -l)"
+  fi
+}
 
 case $os_type in
   debian)
-    #stuff
+    discern_debvers
+    if $EOL; then
+      security_updates=999
+      all_updates=999
+    else
+      debian_check_updates
+    fi
     ;;
   redhat)
     #stuff
@@ -92,8 +134,6 @@ case $os_type in
     ;;
 esac
 
-if [ "$os_type" == "debian" ]; then
-  discern_debvers
-  echo "distro is $distro, version is $distrovers, EOL is $EOL"
-  echo $distro
-fi
+echo "EOL status is $EOL, security errata support is $errata_support, distro is $distro, distrovers is $distrovers, security update count is
+$security_updates, all updates count is $all_updates"
+
